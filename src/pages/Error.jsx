b10a8a9fb33dabe0789b616bdc9bde0a2ca7d874
Denis <denison4@mail.ru>
2022-03-10 20:10:49 +0300

    push commit

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>Такой страницы не существет</h1>
+    </div>
+  );
+};
+
+export default Error;
